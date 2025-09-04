06fa38794e6efd7f898a0d1b4ee1161719bb118c
hyperneutr0n <randylemuel21@gmail.com>
2025-09-04 19:24:55 +0700

    feat: added reset command to reset users table

--- handler_user.go
+++ handler_user.go
@@ -13,11 +13,11 @@ func handlerLogin(s *state, cmd command) error {
 
 	user, err := s.db.GetUser(context.Background(), cmd.Args[0])
 	if err != nil {
-		return errors.New("user " + cmd.Args[0] + " not found" + "\n" + err.Error())
+		return fmt.Errorf("user " + cmd.Args[0] + " not found: %w", err)
 	}
 	
 	if err := s.cfg.SetUser(user.Name); err != nil {
-		return err
+		return fmt.Errorf("failed setting user to config file: %w", err)
 	}
 	fmt.Println("User " + user.Name + " been set. Login success!")
 	
@@ -31,13 +31,22 @@ func handlerRegister(s *state, cmd command) error {
 	
 	user, err := s.db.CreateUser(context.Background(), cmd.Args[0])
 	if err != nil {
-		return errors.New("failed to register user " + cmd.Args[0] + "\n" + err.Error())
+		return fmt.Errorf("failed to register user: %w", err)
 	}
 
 	if err := s.cfg.SetUser(user.Name); err != nil {
-		return errors.New("failed to set user to config file" + "\n" + err.Error())
+		return fmt.Errorf("failed to set user to config file: %w", err)
 	}
 
 	fmt.Println("User " + user.Name + " has been created")
+	return nil
+}
+
+func handlerReset(s *state, cmd command) error {
+	if err := s.db.ResetUser(context.Background()); err != nil {
+		return fmt.Errorf("failed resetting user table: %w", err)
+	}
+	
+	fmt.Println("Successfully resetting user table")
 	return nil
 }
